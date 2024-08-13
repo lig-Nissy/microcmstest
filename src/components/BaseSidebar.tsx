338ba93aa9e4eb00f5b83db2168e8bb0227f2225
nishizawa Yuya <nishizawa_yuya@liginc.co.jp>
2024-08-13 15:21:04 +0900

    feat:サイドバーを検索対象外に指定

--- src/components/BaseSidebar.tsx
+++ src/components/BaseSidebar.tsx
@@ -1,6 +1,6 @@
 const Sidebar = () => {
   return (
-    <aside className="h-svh">
+    <aside className="h-svh" data-pagefind-ignore>
       <h2 className="mt-5 text-center">コンテンツ</h2>
       <p className="ml-4 mt-5">サイド</p>
     </aside>
